196dda9d68eb3e125afbc5376fd75816e7d6c07d
Diandra2 <diandraedwigesamaral@gmail.com>
2022-09-21 08:41:01 -0300

    21/09

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,12 @@
 <?php
+
 use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\API\MensagemController;
 use App\Http\Controllers\API\TopicoController;
+use App\Http\Controllers\API\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,11 +19,14 @@ use Illuminate\Support\Facades\Route;
 */
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+Route::post('/mensagem', [AuthController::class, 'mensage,']);
+
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 // return $request->user();
 // });
 Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', [AuthController::class, 'update']);
     Route::get('/logout', [AuthController::class, 'logout']);
     Route::resource("topico", TopicoController::class);
-    Route::resource("mensagem", MensagemController::class);
+    Route::resource("mensagem", MensagemController::class)->except("index");
    });
